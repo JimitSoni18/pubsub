7cf83199740e5fe75cf5a85bc05337b57d646b81
jimitsoni18 <jimitsoni18@gmail.com>
2025-10-11 23:56:28 +0530

    updates

--- src/subscriber/mod.rs
+++ src/subscriber/mod.rs
@@ -1,8 +1,10 @@
+use std::{pin::Pin, task::Poll};
+
 use crate::{Message, broker::Broker};
 
-use futures::{SinkExt, StreamExt};
-use tokio::sync::mpsc;
-use tokio_util::codec::{FramedRead, FramedWrite, LengthDelimitedCodec};
+use futures::{SinkExt, Stream, StreamExt};
+use tokio::{net::TcpStream, sync::mpsc};
+use tokio_util::codec::{Framed, FramedRead, FramedWrite, LengthDelimitedCodec};
 
 const CHANNEL_BOUNDED_SIZE: usize = 8;
 
@@ -12,23 +14,71 @@ pub struct Subscriber {
     pub(super) sender: mpsc::Sender<Message>,
 }
 
+pub struct SubscriberFuture {
+    broker: &'static Broker,
+    framed: Framed<TcpStream, LengthDelimitedCodec>,
+    // stream: tokio::net::TcpStream,
+    rx: tokio::sync::mpsc::Receiver<Message>,
+    // tx: tokio_util::sync::PollSender<Message>,
+}
+
+impl Future for SubscriberFuture {
+    type Output = ();
+    fn poll(mut self: Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> Poll<Self::Output> {
+        match Pin::new(&mut self.framed).poll_next(cx) {
+            Poll::Ready(Some(Ok(frame))) => {
+                let cmd_str = str::from_utf8(&frame).unwrap();
+                println!("received command: {cmd_str}");
+                let cmd = parse_cmd(cmd_str).unwrap();
+                // let inputb
+            }
+            Poll::Ready(None) => todo!(),
+            _ => todo!(),
+        }
+
+        // Pin::new(&mut self.framed).poll
+
+        match self.rx.poll_recv(cx) {
+            Poll::Ready(Some(message)) => {}
+            // when and why would a channel close - trace
+            Poll::Ready(None) => {
+                loop {
+                    let poll = self.framed.poll_flush_unpin(cx);
+                    //? should block after channel is closed?
+                    match poll {
+                        Poll::Ready(_) => todo!(),
+                        Poll::Pending => todo!(),
+                    }
+                }
+            }
+            _ => {}
+        }
+
+        Poll::Pending
+    }
+}
+
 impl Subscriber {
     pub async fn listen(broker: &'static Broker, stream: tokio::net::TcpStream) {
         // FIXME: bound
         let (tx, mut rx) = mpsc::channel(CHANNEL_BOUNDED_SIZE);
+        // let (tx2, mut rx2) = mpsc::channel(CHANNEL_BOUNDED_SIZE);
+        let codec = LengthDelimitedCodec::new();
+        // let framed = Framed::new(stream, codec)
         let (reader, writer) = stream.into_split();
         let subscriber = Self {
             id: broker.get_id(),
             sender: tx.clone(),
         };
-        let codec = LengthDelimitedCodec::new();
         let mut framed_read = FramedRead::new(reader, codec.clone());
         let mut framed_write = FramedWrite::new(writer, codec);
         // let mut framed_write_clone = framed_write.clone();
         tokio::spawn(async move {
-            loop {
-                let message = rx.recv().await.unwrap();
+            while let Some(message) = rx.recv().await {
                 framed_write.send(message.message.into()).await.unwrap();
+            }
+            if let Err(why) = framed_write.close().await {
+                eprintln!("failed to close writer: {why}");
             }
         });
         tokio::spawn(async move {
@@ -49,21 +99,19 @@ impl Subscriber {
                     Command::Sub { channels } => {
                         channels.iter().for_each(|ch| {
                             // TODO: how should i handle failures
-                            broker.subscribe(ch.to_string(), &subscriber);
+                            broker.subscribe(ch.to_string(), &subscriber).unwrap();
                         });
                     }
-                    Command::UnSub { channels } => {
-                        match channels {
-                            ChanList::All => {
-                                broker.unsubscribe_all(&subscriber);
-                            }
-                            ChanList::Many(channels) => {
-                                channels.iter().for_each(|ch| {
-                                    broker.unsubscribe(ch.to_string(), &subscriber);
-                                });
-                            }
+                    Command::UnSub { channels } => match channels {
+                        ChanList::All => {
+                            broker.unsubscribe_all(&subscriber);
                         }
-                    }
+                        ChanList::Many(channels) => {
+                            channels.iter().for_each(|ch| {
+                                broker.unsubscribe(ch.to_string(), &subscriber);
+                            });
+                        }
+                    },
                     // TODO: implement
                     Command::PSub { channel_patterns } => unimplemented!(),
                     Command::PUnSub { channel_patterns } => unimplemented!(),
@@ -89,10 +137,12 @@ fn parse_cmd(cmd: &str) -> Result<Command, CommandParsingError> {
     };
     match command {
         "PUB" => {
-            if rest.trim().is_empty() {
+            // FIXME: whitespace is message?
+            let channel_message = rest.trim_start();
+            if channel_message.is_empty() {
                 return Err(CommandParsingError::PubWithNoArgs);
             }
-            let Some((channel, message)) = rest.split_once(SPACE) else {
+            let Some((channel, message)) = channel_message.split_once(SPACE) else {
                 return Err(CommandParsingError::PubWithBadArgs);
             };
             Ok(Command::Pub { channel, message })
